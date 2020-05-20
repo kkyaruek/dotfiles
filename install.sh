c8508af31598d4addfbc9002e000fc0acbf865d2
ibato <yoonhaerim@gmail.com>
2020-05-20 22:14:03 +0900

    Update dotfiles

--- install.sh
+++ install.sh
@@ -5,11 +5,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 cp ./vim/vimrc $HOME/.vimrc
 cp ./git/gitconfig $HOME/.gitconfig
-
-cp ./bash/aliases $HOME/.aliases
-cp ./bash/colorbash $HOME/.colorbash
-cp ./bash/bash_profile $HOME/.bash_profile_tk
-
-cp $HOME/.bash_profile $HOME/.bash_profile.bak
-echo 'source $HOME/.bash_profile_tk' >> $HOME/.bash_profile
-source $HOME/.bash_profile
+cp ./sh/aliases $HOME/.aliases
--- update.sh
+++ update.sh
@@ -2,9 +2,5 @@
 
 cp ./vim/vimrc $HOME/.vimrc
 cp ./git/gitconfig $HOME/.gitconfig
+cp ./sh/aliases $HOME/.aliases
 
-cp ./bash/aliases $HOME/.aliases
-cp ./bash/colorbash $HOME/.colorbash
-cp ./bash/bash_profile $HOME/.bash_profile_tk
-
-source $HOME/.bash_profile
